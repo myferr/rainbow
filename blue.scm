09fb7f574c51e4e785a301d148092a614a64fd37
MyferIsADev <contactme.myfer@protonmail.com>
2025-06-01 13:57:10 -0500

    adjust blue

--- blue.scm
+++ blue.scm
@@ -1,6 +1,40 @@
-(define (print-hello n)
-  (if (> n 0)
-      (begin (display "hello world") (newline) (print-hello (- n 1)))
-      'done))
-
-(print-hello 40)
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
